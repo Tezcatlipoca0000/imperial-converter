99cccdb2f2a3a7210cff9571297a6bd7048635cb
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-07-30 22:27:12 -0500

    made progress

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,42 +2,96 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result,
-      valid = /^\d+\.{1}\d+$|^\d+\/{1}\d+$/g, 
+      valid = /^\d*\.?\d+\/?\d*\.?\d+$|^\.\d+$/g,
+      // /^\d+\.{1}\d+$|^\d+\/{1}\d+$|^\d+\.{1}\d+\/{1}\d+$|^\d+\/{1}\d+\.{1}\d+/g, 
+      // /^\d*\.?\d+\/?\d+\.?\d+$/g,
+      frc = /\//g,
+      // /^\d+\/{1}\d+$|^\d+\.{1}\d+\/{1}\d+$/g,
+      //dec = /[\d+\.{1}\d+]/g,
+      //num = /[\d+]/g,
       invalid = /\D/g; 
     result = input.replace(/[a-z]/gi, '');
     if (result === '') result = '1';
-    if (!valid.test(result) && invalid.test(result)) {
-      result = 'Invalid Number';
-      return result;
-    } else {
-      return result;
+    if (!valid.test(result) && invalid.test(result)) result = 'invalid';
+    if (frc.test(result)) {
+      let idx = result.indexOf('/'),
+        x = result.slice(0,idx),
+        y = result.slice(idx+1);
+      result = Number(x) / Number(y);
     }
+    if (isNaN(result)) {
+      result = 'invalid';
+    } else {
+      result = Number(result);
+    }
+    return result;
   };
   
   this.getUnit = function(input) {
     let result,
       valid = /^gal$|^l$|^mi$|^km$|^lbs$|^kg$/gi,
       num = /[\d+\.{1}\d+]|[\d+\/{1}\d+]|[\d+]/g;
-      //num = /^\d+$/g,
-      //dec = /^\d+\.{1}\d+$/g,
-      //frc = /^\d+\/{1}\d+$/g;
     result = input.replace(num, '');
-    
-    console.log('tst1', result);
     result = result.match(valid);
-    console.log('heyyyy', result);
+    if (result === null) {
+      result = 'invalid';
+    }
+    result = result[0].toLowerCase();
+    if (result === 'l') result = result.toUpperCase();
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+    switch(initUnit) {
+      case 'gal':
+        result = 'L';
+        break;
+      case 'L':
+        result = 'gal';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+      case 'km':
+        result = 'mi';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'kg':
+        result = 'lbs';
+        break;
+      default:
+        break;
+    }
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
+    switch(unit) {
+      case 'gal':
+        result = 'gallons';
+        break;
+      case 'L':
+        result = 'liters';
+        break;
+      case 'mi':
+        result = 'miles';
+        break;
+      case 'km':
+        result = 'kilometers';
+        break;
+      case 'lbs':
+        result = 'pounds';
+        break;
+      case 'kg':
+        result = 'kilograms';
+        break;
+      default:
+        break;
+    }
     return result;
   };
   
@@ -46,7 +100,11 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+    switch(initUnit) {
+      case 'gal':
+        result = initNum * galToL;
+        break;
+    }
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,15 @@ module.exports = function (app) {
     .get((req, res) => {
       let input = req.query;
       console.log('route get api/convert... user input ----', input);
-      let num = convertHandler.getNum(input.input);
-      console.log('route get api/convert... num', num);
-      let unit = convertHandler.getUnit(input.input);
+      let initNum = convertHandler.getNum(input.input);
+      console.log('route get api/convert... num', initNum, typeof initNum);
+      let initUnit = convertHandler.getUnit(input.input);
+      console.log('route get api/convert... initUnit', initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      console.log('route get api/convert... returnUnit', returnUnit);
+      let initUnitSO = convertHandler.spellOutUnit(initUnit);
+      let returnUnitSO = convertHandler.spellOutUnit(returnUnit);
+      console.log('route get api/convert... spelledOut init & retr', initUnitSO, returnUnitSO);
     });
 
 };
