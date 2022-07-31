5c776311573580f2cf6561be493e8d991b597d5f
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-07-31 14:24:27 -0500

    made progress

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -30,13 +30,14 @@ function ConvertHandler() {
   this.getUnit = function(input) {
     let result,
       valid = /^gal$|^l$|^mi$|^km$|^lbs$|^kg$/gi,
-      num = /[\d+\.{1}\d+]|[\d+\/{1}\d+]|[\d+]/g;
+      num = /[\d*\.{1}\d+]|[\d+\/{1}\d+]|[\d+]/g;
     result = input.replace(num, '');
     result = result.match(valid);
     if (result === null) {
       result = 'invalid';
+    } else {
+      result = result[0].toLowerCase();
     }
-    result = result[0].toLowerCase();
     if (result === 'l') result = result.toUpperCase();
     return result;
   };
@@ -104,13 +105,31 @@ function ConvertHandler() {
       case 'gal':
         result = initNum * galToL;
         break;
+      case 'L':
+        result = initNum / galToL;
+        break;
+      case 'mi':
+        result = initNum * miToKm;
+        break;
+      case 'km':
+        result = initNum / miToKm;
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+      case 'kg':
+        result = initNum / lbsToKg;
+        break;
+      default:
+        break;
     }
+    console.log('tst1', result, typeof result, result.toString().length);
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+    result = `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -9,17 +9,15 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get((req, res) => {
-      let input = req.query;
-      console.log('route get api/convert... user input ----', input);
-      let initNum = convertHandler.getNum(input.input);
-      console.log('route get api/convert... num', initNum, typeof initNum);
-      let initUnit = convertHandler.getUnit(input.input);
-      console.log('route get api/convert... initUnit', initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      console.log('route get api/convert... returnUnit', returnUnit);
-      let initUnitSO = convertHandler.spellOutUnit(initUnit);
-      let returnUnitSO = convertHandler.spellOutUnit(returnUnit);
-      console.log('route get api/convert... spelledOut init & retr', initUnitSO, returnUnitSO);
+      let input = req.query,
+        initNum = convertHandler.getNum(input.input),
+        initUnit = convertHandler.getUnit(input.input),
+        returnUnit = convertHandler.getReturnUnit(initUnit),
+        initUnitSO = convertHandler.spellOutUnit(initUnit),
+        returnUnitSO = convertHandler.spellOutUnit(returnUnit),
+        returnNum = convertHandler.convert(initNum, initUnit),
+        string = convertHandler.getString(initNum, initUnitSO, returnNum, returnUnitSO);
+      res.json({initNum: initNum, initUnit: initUnit, returnNum: returnNum, returnUnit: returnUnit, string: string});
     });
 
 };
