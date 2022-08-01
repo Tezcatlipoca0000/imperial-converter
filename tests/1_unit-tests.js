d81dbab739dcfa97f76934b6922704d18fd5e309
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-07-31 22:11:41 -0500

    progress unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,29 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+    
+    test('Should correctly read a whole number input.', function() {
+        //let num = convertHandler.getNum('5');
+        //assert.isNotNaN(num, `${num} is not NaN`);
+        let input = '5',
+            returnNum = convertHandler.getNum(input);
+        assert((Number.isInteger(Number(input)) && !isNaN(returnNum)), 'input is a whole number and returnNum is not NaN');
+    });
+
+    test('should correctly read a decimal number input.', function() {
+        let input = '5.5',
+            returnNum = convertHandler.getNum(input);
+        assert((Number(input) && !Number.isInteger(Number(input)) && !isNaN(returnNum)), 'input is a number, is not a whole number and returnNum is not NaN')
+    });
+
+    test('should correctly read a fractional input.', function() {
+        let input = '5/5',
+            frc = /^\d+\/?\d*\.?\d+$/g,
+            returnNum = convertHandler.getNum(input);
+        assert.match(input, frc, 'input matches fraction regexp');
+        assert.isNumber(returnNum, 'returnNum is number');
+    });
+
+    test('should correctly read a fractional input with a decimal.', function() {});
 
 });
